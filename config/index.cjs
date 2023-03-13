a6a597b67abf37c82e6af0e748a5cbb533c02b6b
feiniaosha <55866682+feiniaosha@users.noreply.github.com>
2023-03-13 11:32:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,13 +23,13 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otTsz6d-8T0cxQUGVz4dgTBFHSbw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'yS6382fV9b50j_yPNfTIp2sMwJvZEqB1_Efo5Iivfbo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-14',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1995', date: '08-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
