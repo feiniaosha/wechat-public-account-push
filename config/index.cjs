119fa3717ad969ff9b1f3bfa52e7bb012cbee1b7
feiniaosha <55866682+feiniaosha@users.noreply.github.com>
2023-03-13 16:55:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'otTsz6e0JYuX2TObPjF0R1SiS3qI',
+      id: 'otTsz6d-8T0cxQUGVz4dgTBFHSbw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'H5vQ27GQ0mHp0vWxqNgRwZe3zD5hNEz1P8USUznNRZk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
